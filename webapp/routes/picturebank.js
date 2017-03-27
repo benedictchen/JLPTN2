8f8aa98b659f251e236e33f45b478e1ab2a5ae43
Benedict Chen <benedict@benedictchen.com>
2017-03-28 07:43:22 +0900

    Adding romaji support for api.

--- webapp/routes/picturebank.js
+++ webapp/routes/picturebank.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var pictureData = require('../data/giant-hashmap.json');
-
+var weakkana = require('../public/javascripts/weakkana');
 
 /* GET users listing. */
 router.get('/:word', function(req, res, next) {
+
   var words = req.params.word.split(',');
   var results = [];
   words.forEach((word)=>{
-  	results = results.concat(pictureData[word] || []);
+  	console.log(weakkana.toKana(word))
+  	var wordResults = pictureData[word] || pictureData[weakkana.toKana(word)] || [];
+  	console.log(wordResults);
+  	results = results.concat(wordResults || []);
   });
   res.send({results:results});
 });
