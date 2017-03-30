a03c99ec3a50cf410e5a2767fe1267f12661cc60
Benedict Chen <benedict@benedictchen.com>
2017-03-30 09:30:24 +0900

    Adding elapsed time to remaining.

--- scraper.js
+++ scraper.js
@@ -58,36 +58,46 @@ var totalCount = wordsToScrape.length;
 var startTime = new Date();
 var wordsRetrieved = 0;
 
+
+function getFriendlyTime(totalSeconds) {
+	// get total seconds between the times
+	var delta = totalSeconds;
+
+	// calculate (and subtract) whole days
+	var days = Math.floor(delta / 86400);
+	delta -= days * 86400;
+
+	// calculate (and subtract) whole hours
+	var hours = Math.floor(delta / 3600) % 24;
+	delta -= hours * 3600;
+
+	// calculate (and subtract) whole minutes
+	var minutes = Math.floor(delta / 60) % 60;
+	delta -= minutes * 60;
+
+	// what's left is seconds
+	var seconds = delta % 60; 
+	return `[ ${isNaN(days) ? '?' : days } Days, ` + 
+				`${isNaN(hours) ? '?' : hours} Hours, ` + 
+				`${isNaN(minutes) ? '?' : minutes} Minutes, ` + 
+				`and ${isNaN(seconds) ? '?' : seconds} Seconds `;
+}
+
+
 function process() {
 	var elapsedTime = (new Date().getTime()) - startTime;
 	var wordsPerTime = wordsRetrieved / elapsedTime;
 	var estimatedTotalTime = totalCount / wordsPerTime;
 	var timeLeftInSeconds = (estimatedTotalTime - elapsedTime) / 1000;
-	var delta = timeLeftInSeconds;
+	var elapsedTimeSeconds = elapsedTime / 1000;
 	
-	var days = Math.floor(delta / 86400);
-	delta -= days * 86400;
-	// calculate (and subtract) whole hours
-	var hours = Math.floor(delta / 3600) % 24;
-	delta -= hours * 3600;
-	// calculate (and subtract) whole minutes
-	var minutes = Math.floor(delta / 60) % 60;
-	delta -= minutes * 60;
-	// what's left is seconds
-	var seconds = (delta % 60).toFixed(2);
-
 	console.log('Grabbing from: ' + wordsToScrape.length);
 	var currentWord = wordsToScrape.shift();
 
 	var remaining = ` ${(wordsToScrape.length / totalCount).toFixed(2)}% (${wordsToScrape.length} of ${totalCount})`;
 	console.log('current word: ' + currentWord + remaining);
-	console.log(`>> Time Remaining: ` + 
-				`[ ${isNaN(days) ? '?' : days } Days, ` + 
-				`${isNaN(hours) ? '?' : hours} Hours, ` + 
-				`${isNaN(minutes) ? '?' : minutes} Minutes, ` + 
-				`and ${isNaN(seconds) ? '?' : seconds} Seconds ` + 
-				`] left.`);
-
+	console.log(`Elapsed Time: ` + getFriendlyTime(elapsedTimeSeconds)) + ` have passed.`;
+	console.log(`Time Remaining: ` + getFriendlyTime(timeLeftInSeconds)) + `left.`;
 
 	if (currentWord) {
 		var writePath = 'results/' + currentWord;
